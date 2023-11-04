3269c56a5f5da1a5adbbf9bda7a8248db0159cd4
Jhojan Tobón <jhojantobonm@gmail.com>
2023-11-04 18:15:45 -0500

    Improved loading skeleton animation

--- src/TodosLoading/index.js
+++ src/TodosLoading/index.js
@@ -2,8 +2,7 @@ import './TodosLoading.css'
 
 function TodosLoading(){
   return (
-    // <p className="message message-loading">Loading...</p>
-      <li className="loading loading-li"></li>
+      <li className='loading loading-li'></li>
   )
 }
 
